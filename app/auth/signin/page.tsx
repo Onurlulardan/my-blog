50977b29b4b1c315a2ee42deb3d5a7feab060a8a
Onurlulardan <altuntasonur@outlook.com.tr>
2023-07-09 16:15:12 +0300

    git auth button fix

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -1,12 +1,19 @@
 import { GithubAuthButton } from "@/components/button";
 import { NextPage } from "next";
+import { signIn } from "next-auth/react";
 
 interface Props {}
 
 const Signin: NextPage = () => {
-    return <div className="h-screen flex items-center justify-center bg-primary dark:bg-primary-dark">
-        <GithubAuthButton />
+  return (
+    <div className="h-screen flex items-center justify-center bg-primary dark:bg-primary-dark">
+      <GithubAuthButton
+        onClick={() => {
+          signIn("github");
+        }}
+      />
     </div>
-}
+  );
+};
 
 export default Signin;
