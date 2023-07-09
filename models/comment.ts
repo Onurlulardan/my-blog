89e55ce02e63c4bc5ce4443617cf1af84490f432
Onurlulardan <altuntasonur@outlook.com.tr>
2023-07-09 19:45:34 +0300

    model update

--- models/comment.ts
+++ models/comment.ts
@@ -17,6 +17,7 @@ const CommentSchema = new Schema<IComment>(
     belongsTo: {
       type: Schema.Types.ObjectId,
       ref: "Post",
+      required: true,
     },
     owner: {
       type: Schema.Types.ObjectId,
