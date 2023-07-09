552c41f199864acc3aa752c6ece17d89857f77e8
Onurlulardan <altuntasonur@outlook.com.tr>
2023-07-09 20:05:38 +0300

    yorum api yorum oluşturma eklendi

--- lib/validator.ts
+++ lib/validator.ts
@@ -1,44 +1,60 @@
 import Joi, { ObjectSchema } from "joi";
+import { isValidObjectId } from "mongoose";
 
 export const errorMessages = {
-    INVALID_TITLE: "Title is Missing!",
-    INVALID_TAGS: "Tags must be array of string!",
-    INVALID_SLUG: "Slug is missing!",
-    INVALID_META: "Meta description is missing!",
-    INVALID_CONTENT: "Post content is missing!",
-}
+  INVALID_TITLE: "Title is Missing!",
+  INVALID_TAGS: "Tags must be array of string!",
+  INVALID_SLUG: "Slug is missing!",
+  INVALID_META: "Meta description is missing!",
+  INVALID_CONTENT: "Post content is missing!",
+};
 
 export const postValidationSchema = Joi.object().keys({
-    title: Joi.string().required().messages({
-        "string.empty": errorMessages.INVALID_TITLE,
-        "any,required": errorMessages.INVALID_TITLE,
-    }),
-    content: Joi.string().required().messages({
-        "string.empty": errorMessages.INVALID_CONTENT,
-        "any,required": errorMessages.INVALID_CONTENT,
-    }),
-    slug: Joi.string().required().messages({
-        "string.empty": errorMessages.INVALID_SLUG,
-        "any,required": errorMessages.INVALID_SLUG,
-    }),
-    meta: Joi.string().required().messages({
-        "string.empty": errorMessages.INVALID_META,
-        "any,required": errorMessages.INVALID_META,
-    }),
-    tags: Joi.array().items(Joi.string()).messages({
-        "string.base": errorMessages.INVALID_TAGS,
-        "string,empty": errorMessages.INVALID_TAGS,
-    }),
+  title: Joi.string().required().messages({
+    "string.empty": errorMessages.INVALID_TITLE,
+    "any,required": errorMessages.INVALID_TITLE,
+  }),
+  content: Joi.string().required().messages({
+    "string.empty": errorMessages.INVALID_CONTENT,
+    "any,required": errorMessages.INVALID_CONTENT,
+  }),
+  slug: Joi.string().required().messages({
+    "string.empty": errorMessages.INVALID_SLUG,
+    "any,required": errorMessages.INVALID_SLUG,
+  }),
+  meta: Joi.string().required().messages({
+    "string.empty": errorMessages.INVALID_META,
+    "any,required": errorMessages.INVALID_META,
+  }),
+  tags: Joi.array().items(Joi.string()).messages({
+    "string.base": errorMessages.INVALID_TAGS,
+    "string,empty": errorMessages.INVALID_TAGS,
+  }),
 });
 
+export const commentValidationSchema = Joi.object().keys({
+  belongsTo: Joi.string()
+    .required()
+    .custom((value, helper) => {
+      if (!isValidObjectId(value)) return helper.error("any.invalid");
+      return true;
+    })
+    .messages({
+      "any.invalid": "Post id should be presented as belongsTo!",
+      "string.empty": "Invalid belongsTo!",
+    }),
+  content: Joi.string().required().messages({
+    "string.empty": "Content is missing inside comment!",
+  }),
+});
 
 export const validateSchema = (schmea: ObjectSchema, value: any) => {
-    const { error } = schmea.validate(value, {
-        errors: { label: "key", wrap: { label: false, array: false } },
-        allowUnknown: true,
-    });
+  const { error } = schmea.validate(value, {
+    errors: { label: "key", wrap: { label: false, array: false } },
+    allowUnknown: true,
+  });
 
-    if (error) return error.details[0].message;
+  if (error) return error.details[0].message;
 
-    return "";
-}
+  return "";
+};
--- pages/api/comment/index.ts
+++ pages/api/comment/index.ts
@@ -1,3 +1,7 @@
+import dbConnect from "@/lib/dbConnect";
+import { commentValidationSchema, validateSchema } from "@/lib/validator";
+import Comment from "@/models/comment";
+import Post from "@/models/post";
 import { isAuth } from "@/utils/helper";
 import { NextApiHandler } from "next";
 
@@ -14,8 +18,26 @@ const handler: NextApiHandler = (req, res) => {
 
 const createNewComment: NextApiHandler = async (req, res) => {
   const user = await isAuth(req, res);
-  console.log(user);
   if (!user) return res.status(403).json({ error: "Unauthorized request!" });
+
+  const error = validateSchema(commentValidationSchema, req.body);
+  if (error) return res.status(422).json({ error });
+
+  //create Comment
+  await dbConnect();
+  const { belongsTo, content } = req.body;
+  const post = await Post.findById(belongsTo);
+  if (!post) return res.status(401).json({ error: "Invalid Post!" });
+
+  const comment = new Comment({
+    content,
+    belongsTo,
+    owner: user.id,
+    chiefComment: true,
+  });
+
+  await comment.save();
+  res.status(201).json(comment);
 };
 
 export default handler;
